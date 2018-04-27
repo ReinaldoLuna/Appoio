ff187de821a9029438db84bc6120d930a5953f84
Reinaldo <lunajf18@gmail.com>
2018-04-26 22:30:36 -0300

    Tratando erros com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,28 +1,81 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
+import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
             .catch((error, caught) => {
 
                 let errorObj = error;
-                if (errorObj.error){
+                if (errorObj.error) {
                     errorObj = errorObj.error;
                 }
 
-                if(!errorObj.status){
+                if (!errorObj.status) {
                     errorObj = JSON.parse(errorObj);
                 }
 
-                 console.log("Erro detectado pelo interceptor: ");
-                 console.log(errorObj);
+                console.log("Erro detectado pelo interceptor: ");
+                console.log(errorObj);
+
+                switch (errorObj.status) {
+
+                    case 401:
+                        this.handle401();
+                        break;
+
+                    case 403:
+                        this.handle403();
+                        break;
+
+                    default:
+                        this.handleDefaultError(errorObj);
+                        break;
+                }
 
                 return Observable.throw(errorObj)
             }) as any
+    }
+
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: "Falha de autenticação",
+            message: "Email ou senha incorretos!",
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: "Ok"
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handle403() {
+        this.storage.setLocalUser(null)
+    }
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({ 
+            title: "Erro " + errorObj.status + ": " + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: "Ok"
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -12,7 +12,7 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class PerfilPage {
 
-  usuario: UsuarioDTO 
+  usuario: UsuarioDTO
 
   constructor(
     public navCtrl: NavController,
@@ -24,21 +24,29 @@ export class PerfilPage {
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email){
-       this.usuarioService.findByEmail(localUser.email)
-       .subscribe(response => {
-         this.usuario = response;
-         //IMPORTANTE: buscar imagem
-         this.getImageIfExists()
-       }, error => {})
+    if (localUser && localUser.email) { 
+      this.usuarioService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.usuario = response;
+          this.getImageIfExists()
+        }, error => {
+          console.log("Erro: " + error)
+          if (error.status == 403) {
+            console.log("Deu erro 403")
+            this.navCtrl.setRoot('LoginPage')
+          }
+        })
+    } else {
+
+      this.navCtrl.setRoot('LoginPage')
     }
   }
 
-  getImageIfExists(){
+  getImageIfExists() {
     this.usuarioService.getImageFromBucket(this.usuario.id)
-    .subscribe(respose => {
-      this.usuario.foto = `${ API_CONFIG.bucketBaseUrl }/cp${ this.usuario.id }.jpg`
-    }, error => {})
+      .subscribe(respose => {
+        this.usuario.foto = `${API_CONFIG.bucketBaseUrl}/cp${this.usuario.id}.jpg`
+      }, error => { })
   }
 
 }
